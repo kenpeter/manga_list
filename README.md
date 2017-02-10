c0b319b16e4616dd73a461e9844d9e12fa6eef8f
kenpeter <figo2476@hotmail.com>
2017-02-10 15:27:32 +1100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Intro
+
+Try to use React, Semantic UI and http://www.mangaeden.com/api to build a simple app, to list manga.
+
+* Use this as a base, https://github.com/vasanthk/react-es6-webpack-boilerplate
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+
+// webpack
+const webpack = require('webpack');
+
+// path
+const path = require('path');
+
+
+// module exports
+module.exports = {
+  // entry, multiple entries
+  entry: [
+    // a little express server. Serve the client at http://localhost:5000
+    "webpack-dev-server/client?http://localhost:5000",
+    // webpack-hot-dev-server
+    "webpack/hot/dev-server",
+    // ./scripts/index.js
+    "./client/index"
+  ],
+  
+  // output, virtual output
+  output: {
+    // path, output to current dir
+    path: __dirname,  
+    filename: "bundle.js", // __dirname/bundle.js
+    publicPath: "/static/" // load css, js, etc
+  },
+  
+  // resolve file extension
+  resolve: {
+    extension: ["", ".js"]
+  },
+
+  // see source map
+  devtool: "eval-source-map",
+
+  plugins: [
+    // able to add remove npm package, with no reloading
+    new webpack.HotModuleReplacementPlugin(),
+    // Basically, no error emit, when there is an error. Print error to console only.
+    new webpack.NoErrorsPlugin()
+  ],
+  
+  // module
+  module: {
+    // loaders
+    loaders: [
+      {
+        // test, .jsx, what about .js
+        test: /\.jsx?$/,
+        // loaders, babel
+        loaders: ['babel'],
+        // include, means include dirs....?
+        //include: path.join(__dirname, 'scripts')
+        include: path.join(__dirname, 'client')
+      }
+    
+    ]
+  }
+
+};
